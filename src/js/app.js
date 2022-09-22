506b8e7362aa6aa456c6377afedb7be24c7ef2d6
David19782 <yordanovdavid@gmail.com>
2022-09-22 16:27:01 +0300

    feat: correct app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,11 @@
 import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
+    let curr = document.querySelectorAll(".hot");
+
+    let emoji = "&#x1F525";
+    for(let i = 0; i < curr.length; i++){ 
+      let c = curr[i].innerHTML;
+      curr[i].innerHTML = c + emoji;
+    }
 });
