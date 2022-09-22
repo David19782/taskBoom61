c001bd064db92564e738aef32af9931543497180
David19782 <yordanovdavid@gmail.com>
2022-09-22 14:28:01 +0300

    feat: correct app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,12 @@
 import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
-  // This block will be executed once the page is loaded and ready
+  let curr = document.getElementsByClassName("hot");
+      let emoji = "&#x1F525";
+      for(let i = 0; i < curr.length; i++){
+        
+        let c = curr[i].innerHTML;
+        curr[i].innerHTML = c + emoji;
+      
+      }
 });
